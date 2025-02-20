5c499bd7d2d458ae5d2ae9dfb5483aa34c0daa52
RunAtTekky <varunrawat343@gmail.com>
2025-02-20 15:32:07 +0530

    STL functions

--- a.cpp
+++ a.cpp
@@ -2,9 +2,39 @@
 using namespace std;
 
 void solve() {
-    int n,m; cin >> n >> m;
+    int n; cin >> n;
+    vector<int> vec(n);
+    for (auto &ele : vec) cin >> ele;
+    
 
-    cout << m + 1 << "\n";
+    // # Find if
+    // auto it = find_if(vec.begin(), vec.end(), [&](int x) {
+    //     return x&1;
+    // });
+
+    // cout << it - vec.begin() << "\n";
+
+    // # Count if
+    // auto count = count_if(vec.begin(), vec.end(), [&](int x) {
+    //     return x&1;
+    // });
+
+    // cout << count << "\n";
+
+    // # Unique
+    // sort(vec.begin(), vec.end());
+
+    // auto it = unique(vec.begin(), vec.end());
+
+    // vec.resize(it-vec.begin());
+
+    // for (auto &ele : vec) cout << ele << " ";
+    // cout << "\n";
+    
+    
+
+    
+
 }
 
 int main() {
